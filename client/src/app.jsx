8128ae27fbacb681e7b88bc16922060ca9c347ae
minhyeok <mahns1201@gmail.com>
2022-09-23 17:52:12 +0900

    [init] react

---
+++ client/src/app.jsx
@@ -0,0 +1,29 @@
+import './styles/app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <nav className="layout">
+          <h1>LOGO</h1>
+          <ul>
+            <li>TAB1</li>
+            <li>TAB2</li>
+            <li>TAB3</li>
+          </ul>
+          <ul>
+            <li>로그인</li>
+            <li>로그아웃</li>
+            <li>회원가입</li>
+          </ul>
+        </nav>
+      </header>
+
+      <div className="contents">test</div>
+
+      <footer>footer</footer>
+    </div>
+  );
+}
+
+export default App;
