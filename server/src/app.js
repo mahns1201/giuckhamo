5d53c1a3e11bf922ac95b788e27e2309547b96ee
서민혁 <mahns1201@gmail.com>
2022-10-03 05:41:31 +0900

    [temp]

--- server/src/app.js
+++ server/src/app.js
@@ -13,10 +13,13 @@ const app = fastify({
 app.register(cors, { origin: '*' });
 app.register(homeRoutes);
 
-app.listen({ port: process.env.PORT || 5000 }, function (err, address) {
-  if (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-  // Server is now listening on ${address}
-});
+app.listen(
+  { port: process.env.PORT || 5000, host: process.env.HOST || '0.0.0.0' },
+  function (err, address) {
+    if (err) {
+      app.log.error(err);
+      process.exit(1);
+    }
+    // Server is now listening on ${address}
+  },
+);
