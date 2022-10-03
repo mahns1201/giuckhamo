c9e33fab714c49d9eae85b90995632db05a9b690
서민혁 <mahns1201@gmail.com>
2022-10-04 00:07:24 +0900

    [refactor] directory, simple style

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,45 +1,16 @@
 import './styles/app.scss';
 
-import Header from './components/common/header';
-import Footer from './components/common/footer';
+import Header from './components/header';
+import Map from './components/map';
+import Footer from './components/footer';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-
-      <div className="contents">
-        <div className="test1">
-          <a href="https://mahns-dev.notion.site/da619d4ed60a4d89af20af2593fc0cc2">
-            링크
-          </a>
-        </div>
-
-        <div className="test2">
-          <a href="https://mahns-dev.notion.site/da619d4ed60a4d89af20af2593fc0cc2">
-            링크
-          </a>
-        </div>
-
-        <div className="test3">
-          <a href="https://mahns-dev.notion.site/da619d4ed60a4d89af20af2593fc0cc2">
-            링크
-          </a>
-        </div>
-
-        <div className="test4">
-          <a href="https://mahns-dev.notion.site/da619d4ed60a4d89af20af2593fc0cc2">
-            링크
-          </a>
-        </div>
-
-        <div className="test5">
-          <a href="https://mahns-dev.notion.site/da619d4ed60a4d89af20af2593fc0cc2">
-            링크
-          </a>
-        </div>
-      </div>
-
+      <section className="contents">
+        <Map />
+      </section>
       <Footer />
     </div>
   );
