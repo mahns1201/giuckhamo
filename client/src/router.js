df7c708bce7e9cd0df19e0edd207810abced10ca
minhyeok <mahns1201@gmail.com>
2022-10-09 22:45:59 +0900

    [feat] admin-login set route

--- client/src/router.js
+++ client/src/router.js
@@ -1,10 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './app';
+import Login from './pages/admin/login';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+  },
+
+  {
+    path: '/admin/login',
+    element: <Login />,
   },
 ]);
 
